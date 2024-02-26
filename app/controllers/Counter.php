562a6c544ece0fde723a17d119ee2bbcf42aee72
Alexander Cécile <35971201+AlexanderCecile@users.noreply.github.com>
2024-02-25 22:15:52 -0500

    Started work on counter, confused by role of AJAX.

--- app/controllers/Counter.php
+++ app/controllers/Counter.php
@@ -4,6 +4,11 @@ namespace app\controllers;
 use stdClass;
 
 class Counter extends \app\core\Controller {
-	function index() {}
+	function index() {
+		$counter_obj = new \app\models\Counter;
+		$counter_obj->increment();
+		$counter_obj->write();
+		$this->view('Shared/counter', $counter_obj);
+	}
 }
 ?>
--- app/models/Counter.php
+++ app/models/Counter.php
@@ -3,9 +3,32 @@ namespace app\models;
 
 class Counter {
 	public $count;
-	public function __construct() {}
-	public function increment() {}
-	public function write() {}
-	public function __toString() {}
+	public function __construct() {
+		if (file_exists('/../../resources/counter.txt')) {
+			$count_raw = file_get_contents('/../../resources/counter.txt');
+		}
+		else {
+			$count_raw = '{"count":0}';
+		}
+
+		# for the associative: false part, see https://www.php.net/manual/en/function.json-decode.php#126787
+		# TODO: make the correction for the other uses of JSON in the program
+		$this->count = json_decode($count_raw, associative: false)->count;
+	}
+
+	public function increment() {
+		$this->count = $this->count + 1;
+	}
+	public function write() {
+		$handle = fopen('/../../resources/counter.txt', mode: 'w');
+		flock($handle, LOCK_EX);
+		$data = json_encode($this, associative: false);
+		fwrite($data);
+		flock($handle, LOCK_UN);
+		fclose($handle);
+	}
+	public function __toString() {
+		return json_encode($this, associative: false);
+	}
 }
 ?>
